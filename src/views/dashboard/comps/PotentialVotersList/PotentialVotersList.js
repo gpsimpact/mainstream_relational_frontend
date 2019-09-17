93fd1ecc370ef7ec6d48ae51e5a0c2a7f05b3a8c
joshuamitchell <joshua@gpsimpact.com>
2019-09-17 10:13:30 -0500

    start on pagination, rename voter props, votermatch conditionals

--- src/views/dashboard/comps/PotentialVotersList/PotentialVotersList.js
+++ src/views/dashboard/comps/PotentialVotersList/PotentialVotersList.js
@@ -1,54 +1,71 @@
 import React, { PureComponent } from 'react';
 import { withRouter } from 'react-router-dom';
-import { Query } from 'react-apollo';
+import { Query, withApollo } from 'react-apollo';
 
 import { Intro } from '../Intro/intro';
 import MY_POTENTIAL_VOTERS from '../../../../data/queries/potentialVoters';
+
 // import VoterSearchModal from './comps/Modals/VoterSearchModal';
 import IndividualVoter from '../IndividualVoter/IndividualVoter';
-import IndividualVoterModal from '../Modals/individualVoterModal';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
+import { VoterPageInfoComp } from '../../VoterPageInfo';
+import { Pagination } from '../Pagination';
 
 class PotentialVotersList extends PureComponent {
     render() {
         return (
-            <Query
-                query={MY_POTENTIAL_VOTERS}
-                variables={{
-                    limit: 25,
-                    org_id: this.props.match.params.orgSlug,
-                }}
-            >
-                {({ data: { potentialVoters }, loading, error }) => {
-                    if (loading) {
-                        return <p>Loading...</p>;
-                    }
-                    else if (error) {
-                        return <p>Error!</p>;
-                    }
-                    if (potentialVoters.items.length === 0) {
-                        return <Intro />
-                    }
+            <VoterPageInfoComp>
+                {({data: {voterPageInfo}}) => {
+                    return(
+                            <Query
+                                query={MY_POTENTIAL_VOTERS}
+                                variables={{
+                                    limit: 25,
+                                    org_id: this.props.match.params.orgSlug,
+                                }}
+                          
+                            >
+                                {({ data: { potentialVoters }, loading, error }) => {
+                                    if (loading) {
+                                        return <p>Loading...</p>;
+                                    }
+                                    else if (error) {
+                                        return <p>Error!</p>;
+                                    }
+                                    if (potentialVoters.items.length === 0) {
+                                        return <Intro />
+                                    }
 
-                    // console.log(potentialVoters.items);
+                                    // console.log(potentialVoters.items);
 
-                    return (
-                        <div>
-                            {potentialVoters.items.map((item, idx) => {
-                                // console.log(item.first_name);
-                                // break out into individual voter component
-                                return (
-                                    <div>
-                                    <IndividualVoter key={idx} item={item} />
-                                    </div>
-                                );
-                            })}
-                        </div>
-                    );
+                                    return (
+                                        <Row bsPrefix="row">
+                                            <Col md={12}>
+                                                <Pagination voterPageInfo={voterPageInfo} potentialVoters={potentialVoters}/>
+                                            </Col>
+
+                                            {potentialVoters.items.map((item, idx) => {
+                                                // console.log(item.first_name);
+                                                // break out into individual voter component
+                                                return (
+                                                    // adding col div here.  On map items the very first div/component needs to have the key assigned to it.
+                                                    // changing "child" to "voter" 
+                                                    <Col key={idx} md={12}>
+                                                        <IndividualVoter voter={item} />
+                                                    </Col>
+                                                    );
+                                                })}
+                                    </Row>
+                                    );
+                                }}
+                            </Query>
+                        )
                 }}
-            </Query>
+            </VoterPageInfoComp>
         );
     }
 }
 
-export default withRouter(PotentialVotersList);
+export default withApollo(withRouter(PotentialVotersList));
--- src/views/dashboard/index.js
+++ src/views/dashboard/index.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 import { withRouter } from 'react-router-dom';
 import { Routes } from './routes';
+import "./style.css";
 
 class U extends PureComponent {
   render() {
