6f3378a72f1b2c42906733b1b69dbe6f8a7446ca
joshuamitchell <joshua@gpsimpact.com>
2019-10-24 15:19:06 -0500

    updating page info to 25 limit and delete reloads page

--- src/utils/parsePageInfo.js
+++ src/utils/parsePageInfo.js
@@ -1,7 +1,7 @@
 export class PageInfo {
     constructor(params){
         this.params = new URLSearchParams(params);
-        this.limit = 10;
+        this.limit = 25;
         this.pageNumber = this.getPageNumber();
         this.offset=this.calcOffset();
     }
--- src/views/user/DeletePotentialVoterButton.jsx
+++ src/views/user/DeletePotentialVoterButton.jsx
@@ -6,40 +6,25 @@ import UPDATE_PV from "../../data/mutations/modifyPV";
 import { withRouter } from "react-router-dom";
 import ReactRouterPropTypes from "react-router-prop-types";
 import PropTypes from "prop-types";
-import { filter } from "lodash";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faExclamation } from "@fortawesome/fontawesome-free";
 
 export class DeletePotentialVoterButton extends Component {
   render() {
+    console.log(this.props)
     return (
       <Mutation
         mutation={UPDATE_PV}
         variables={{ id: this.props.pv_id, data: { deleted: true } }}
-        update={(store, { data: { updatePotentialVoter } }) => {
-          const data = store.readQuery({
-            query: MY_POTENTIAL_VOTERS,
-            variables: { limit: 25, org_id: this.props.match.params.orgSlug }
-          });
-
-          // remove item from cache
-          data.potentialVoters.items = filter(
-            data.potentialVoters.items,
-            item => item.id !== updatePotentialVoter.id
-          );
-
-          store.writeQuery({
-            query: MY_POTENTIAL_VOTERS,
-            variables: { limit: 25, org_id: this.props.match.params.orgSlug },
-            data
-          });
-        }}
       >
         {updatePotentialVoter => (
           <button
             className="button is-danger"
             onClick={() =>
-              updatePotentialVoter().then(() => this.props.close_modal())
+              updatePotentialVoter().then(() => 
+                window.location = `${this.props.location.pathname}`
+
+              )
             }
           >
             <span className="icon">
